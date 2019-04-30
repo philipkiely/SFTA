ef4773c2c93c22c6a1d33ebbf4fb4e85fab260cd
Philip Kiely <kielyphi@grinnell.edu>
2019-04-30 16:54:32 +0200

    models

--- encryptransfer/files/models.py
+++ encryptransfer/files/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class File(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    base_path = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=150)
+
+
+class AccessController(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    base_path = models.CharField(max_length=150, unique=True)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    server_msn = models.PositiveIntegerField()
+    client_msn = models.PositiveIntegerField()
